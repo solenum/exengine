410378a2f8aa31c7a6f462772a79ce05a2a33d04
exezin <young96steven@gmail.com>
2019-01-31 03:31:58 +0000

    fix physfs for Windows 10 build 1809 (API changes)

--- src/lib/physfs/physfs_platform_windows.c
+++ src/lib/physfs/physfs_platform_windows.c
@@ -575,7 +575,7 @@ char *__PHYSFS_platformCalcUserDir(void)
          *  psize. Also note that the second parameter can't be
          *  NULL or the function fails.
          */
-        rc = pGetDir(accessToken, &dummy, &psize);
+        rc = pGetDir(accessToken, NULL, &psize);
         GOTO_IF(rc, PHYSFS_ERR_OS_ERROR, done);  /* should have failed! */
 
         /* Allocate memory for the profile directory */
